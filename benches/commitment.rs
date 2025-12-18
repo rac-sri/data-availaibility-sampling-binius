3431f000230b84da28138290acc9693093f96285
Toufeeq Pasha <toufeeqpasha.2014@gmail.com>
2025-12-18 16:13:54 +0530

    add kzg commitment benchmarks

--- benches/commitment.rs
+++ benches/commitment.rs
@@ -1,9 +1,62 @@
 use binius_das_poc::{friveil::FriVeilDefault, poly::Utils};
 use divan::Bencher;
+use kate::{
+    M1NoPrecomp,
+    couscous::multiproof_params,
+    gridgen::core::{AsBytes, EvaluationGrid},
+};
 use rand::Rng;
+use std::u16;
 
 fn main() {
     divan::main();
+}
+
+// A simple opinionated function to build KZG commitments over the data
+pub fn kzg_commitment(data: &[u8], with_redundancy: bool, srs: &M1NoPrecomp) -> Vec<u8> {
+    let max_width = srs.powers_of_g1.len();
+    let max_height = u16::MAX as usize;
+
+    let eval_grid = EvaluationGrid::from_data(data, 4, max_width, max_height, Default::default())
+        .expect("valid evaluation grid");
+    let poly_grid = eval_grid
+        .into_polynomial_grid()
+        .expect("valid polynomial grid");
+    let commitments = if with_redundancy {
+        poly_grid
+            .extended_commitments(srs, 2)
+            .expect("kzg commitments")
+    } else {
+        poly_grid.commitments(srs).expect("kzg commitments")
+    };
+
+    // Serialize commitments
+    let mut commitment_bytes = Vec::with_capacity(commitments.len() * 48);
+    for c in commitments {
+        commitment_bytes.extend_from_slice(&c.to_bytes().unwrap());
+    }
+
+    commitment_bytes
+}
+
+#[divan::bench(max_time = 10)]
+fn kzg_16mb_with_redundancy(bencher: Bencher) {
+    let srs = multiproof_params();
+    let real_data = std::fs::read("benches/16MB").unwrap();
+
+    bencher.bench_local(|| {
+        let _ = kzg_commitment(&real_data, true, &srs);
+    });
+}
+
+#[divan::bench(max_time = 10)]
+fn kzg_16mb_without_redundancy(bencher: Bencher) {
+    let srs = multiproof_params();
+    let real_data = std::fs::read("benches/16MB").unwrap();
+
+    bencher.bench_local(|| {
+        let _ = kzg_commitment(&real_data, false, &srs);
+    });
 }
 
 const DATA_32_MIB: usize = 32 * 1024 * 1024;
